618bae8590d3fd361bf8214d875f95b6ee8cf1d3
nithin1992 <nithin.nik@gmail.com>
2014-04-27 12:52:12 +0530

    A few random fixes and changes

---
+++ Login/src/com/sportodemoapp/FatResultDisplay.java
@@ -0,0 +1,70 @@
+package com.sportodemoapp;
+
+import org.json.JSONObject;
+
+import com.sportodemoapp.library.MainDatabaseHandler;
+import com.sportodemoapp.library.UserFunctions;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class FatResultDisplay extends Activity{
+	public String compositeKey;
+	private MainDatabaseHandler dbHelper;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fatlistview);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		Intent intent = getIntent();
+        compositeKey = intent.getStringExtra(FatFragment.COMPOSITE_KEY); 
+        dbHelper = new MainDatabaseHandler(this);
+        displayListView();
+	}
+	
+	
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+	private void displayListView(){
+	
+	
+	
+	}
+	
+	
+	private class FatFetch extends AsyncTask<String, String, JSONObject> {
+		String placeid;
+		@Override
+	    protected void onPreExecute() {
+	        super.onPreExecute();
+	        dbHelper.open();
+			placeid = dbHelper.fetchLocationId(compositeKey);			
+			
+	}
+		
+		 @Override
+	     protected JSONObject doInBackground(String... args) {
+			 UserFunctions userFunction = new UserFunctions();
+	        // JSONObject json = userFunction.searchfat(placeId, game, date)
+			 JSONObject json =null;
+	         return json;
+	     }
+		 
+		 @Override
+	     protected void onPostExecute(JSONObject json) {
+	}
+	
+	}
+}
+
